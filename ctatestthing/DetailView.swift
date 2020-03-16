ff484d4eafe32c3fa0a015ba7219b0f36536d81c
Ahad Islam <ahad.islam624@gmail.com>
2020-03-16 10:44:23 -0400

    added placeholders and new files.

---
+++ ctatestthing/DetailView.swift
@@ -0,0 +1,21 @@
+//
+//  DetailView.swift
+//  ctatestthing
+//
+//  Created by Ahad Islam on 3/16/20.
+//  Copyright © 2020 Ahad Islam. All rights reserved.
+//
+
+import UIKit
+
+class DetailView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
